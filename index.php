5ba2fc200e3c98c4c5ddd19b614eef84576d5b47
icarus94 <icarusrevolution88@gmail.com>
2016-03-24 13:14:04 +0100

    index.php --> html+head,body not yet done

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dusan
- * Date: 24.3.2016.
- * Time: 12.32
- */
+
+?>
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>To do list</title>
+        <meta charset="UTF-8">
+        <meta name="author" content="Trajko,Filip,Otas,Dule legenda">
+        <meta name="description" content="To do list / Lista stvari za uraditi">
+    </head>
+    <body>
+
+    </body>
+    </html>
