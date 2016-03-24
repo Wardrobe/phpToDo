5fa1342640fbd76ea3bd502dbdd17ec35a42c78e
icarus94 <icarusrevolution88@gmail.com>
2016-03-24 17:30:29 +0100

    doradjene forme

--- index.php
+++ index.php
@@ -9,6 +9,19 @@
         <meta name="author" content="Trajko,Filip,Otas,Dule legenda">
         <meta name="description" content="To do list / Lista stvari za uraditi">
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('#datefromparent').click(function() {
+                    // alert($(this).prop('checked'));
+                    if ($(this).is(':checked') == true) {
+                        $('#datesubtask').val("2017-03-23T00:00:50");
+                        $('#datesubtask').prop('disabled', true);
+                    }else{
+                        $('#datesubtask').prop('disabled', false);
+                    }
+                });
+            });
+        </script>
     </head>
     <body>
 
