dc895379469bb373eb2b98db8ad439fd5f127c3f
nikola <icarusrevolution88@gmail.com>
2016-04-01 19:40:27 +0200

    Set as parent radi namesten js i ID

--- SubtaskFormObject.php
+++ SubtaskFormObject.php
@@ -25,12 +25,15 @@ class SubtaskFormObject
                     <br>
                     Date:<br>
                     <input type=\"hidden\" name=\"taskID\" value=".$this->taskParentID.">
-                    <input type=\"hidden\" name=\"datefromparent\" value=".$this->taskParentDate." id=\"parent_date\">
-                    <input type=\"datetime-local\" name=\"date\" min=".$c." id=\"datesubtask\"> or
-                    <input type=\"checkbox\" id=\"datefromparent\" >Same as parent</input>
+                    <input type=\"hidden\" name=\"datefromparent\" value=".$this->taskParentDate." id=\"parent_date".$this->taskParentID."\">
+                    <input type=\"datetime-local\" name=\"date\" min=".$c." id=\"datesubtask".$this->taskParentID."\"> or
+                    <input type=\"checkbox\" id=\"datefromparent".$this->taskParentID."\" onclick=
+                    \"setAsParent('#datefromparent".$this->taskParentID."','#datesubtask".$this->taskParentID."','parent_date".$this->taskParentID."')\">
+                    Same as parent</input>
                     <br>
                     <input type=\"submit\" name=\"submit_subtask\" value=\"Save\">
                     <br>
+                    <script>setAsParent('#datefromparent".$this->taskParentID."','#datesubtask".$this->taskParentID."','parent_date".$this->taskParentID."');</script>
                 </form>";
         }
         return $a;
