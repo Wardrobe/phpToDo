2527331edcead45f81261fd8d7dac02e3d00e8ad
icarus94 <icarusrevolution88@gmail.com>
2016-04-04 22:07:35 +0200

    format datuma promenjen

--- FormCommunication.php
+++ FormCommunication.php
@@ -15,12 +15,17 @@
     if(isset($_POST['submit_subtask'])) {
         $formText = $_POST['task'];
         $formTime = $_POST['date'];
+        $dateArray = explode('T',$formTime);
+        $datefirst= explode('-',$dateArray[0]);
+
+        $dateForDatabase = $datefirst[2]."-".$datefirst[1]."-".$datefirst[0]." ".$dateArray[1].":00";
         $formTaskID = $_POST['taskID'];
         $tasks = unserialize($_SESSION['tasks']);
         $task = $tasks->getTaskByID($formTaskID);
-        $subtask=$task->createNewSubTask($formText,$formTime);
+        $subtask=$task->createNewSubTask($formText,$dateForDatabase);
         $subtask->createNewTaskInDatabase();
         // ovo dalje je u eksperimentalnoj fazi
+        echo $dateForDatabase;
 
         $_SESSION["edited"] = true;
 
