cab05e8f2d4c89a51567ff89f3f207bbeead2af2
Dusan <dulerad94@gmail.com>
2016-04-05 15:12:07 +0200

    Trajko madjija

---
+++ Badge.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Trajko
+ * Date: 5.4.2016.
+ * Time: 15.10
+ */
+
+
+class Badge{
+    public $trajko;
+    public $ledjenda;
+    public $name;
+    public $radimoNaEngleskom;
+}
